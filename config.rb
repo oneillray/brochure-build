9606ce6570426a99d15e8a95567baa65af158207
Luke Murphy <lukewm@riseup.net>
2015-04-03 21:48:10 +0100

    replace static build with a compass/sass-bootstrap build

---
+++ config.rb
@@ -0,0 +1,12 @@
+require 'bootstrap-sass'
+require 'compass/import-once/activate'
+
+http_path        = "/assets"
+css_dir          = "assets/stylesheets"
+sass_dir         = "assets/sass"
+fonts_dir        = "assets/fonts"
+images_dir       = "assets/images"
+javascripts_dir  = "assets/javascripts"
+
+output_style     = :compressed
+preferred_syntax = :sass
