9c81524ff943cbbda1d818752332efb9b62e8d98
Luke Murphy <lukewm@riseup.net>
2015-04-04 17:01:18 +0100

    add bootstrap chevron + update config.rb
    Needed to set 'relative_assets' for Compass to pick
    up where my fonts are.

--- config.rb
+++ config.rb
@@ -1,7 +1,7 @@
 require 'bootstrap-sass'
 require 'compass/import-once/activate'
 
-http_path        = "/assets"
+http_path        = ""
 css_dir          = "assets/stylesheets"
 sass_dir         = "assets/sass"
 fonts_dir        = "assets/fonts"
@@ -9,4 +9,5 @@ images_dir       = "assets/images"
 javascripts_dir  = "assets/javascripts"
 
 output_style     = :compressed
+relative_assets  = :true
 preferred_syntax = :sass
